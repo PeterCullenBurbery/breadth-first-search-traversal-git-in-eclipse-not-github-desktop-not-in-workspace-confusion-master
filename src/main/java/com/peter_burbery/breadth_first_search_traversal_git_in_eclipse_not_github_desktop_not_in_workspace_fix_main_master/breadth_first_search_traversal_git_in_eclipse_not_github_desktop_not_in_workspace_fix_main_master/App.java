1f8d3735be624f83bf7a590c60c7397c8c38481d
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2024-09-25 17:50:56 -0400

    I make a commit.

---
+++ src/main/java/com/peter_burbery/breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace_fix_main_master/breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace_fix_main_master/App.java
@@ -0,0 +1,10 @@
+package com.peter_burbery.breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace_fix_main_master.breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace_fix_main_master;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/peter_burbery/breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace_fix_main_master/breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace_fix_main_master/AppTest.java
@@ -0,0 +1,19 @@
+package com.peter_burbery.breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace_fix_main_master.breadth_first_search_traversal_git_in_eclipse_not_github_desktop_not_in_workspace_fix_main_master;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
